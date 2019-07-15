3e93227e22e1f94c1f3053c07b21d96f36793718
Fausto <faustofrnard@hotmail.com>
2019-07-15 18:47:11 -0500

    Ingresar historia

--- src/consmed/modulos/pacpaciente/view/controller/PacBeanHistoriaC.java
+++ src/consmed/modulos/pacpaciente/view/controller/PacBeanHistoriaC.java
@@ -3,6 +3,9 @@ package consmed.modulos.pacpaciente.view.controller;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.sql.Time;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import javax.annotation.PostConstruct;
@@ -14,7 +17,9 @@ import javax.inject.Named;
 
 import consmed.core.model.entities.MedMedico;
 import consmed.core.model.entities.PacCabeceraHc;
+import consmed.core.model.entities.PacHistoriaClinica;
 import consmed.core.model.entities.PacPaciente;
+import consmed.modulos.medmedico.model.MedManagerMedico;
 import consmed.modulos.pacpaciente.model.PacManagerPaciente;
 import consmed.modulos.resercita.view.controller.ReserBeanCita;
 import consmed.modulos.view.util.JSFUtil;
@@ -22,7 +27,7 @@ import consmed.modulos.view.util.JSFUtil;
 @Named
 @SessionScoped
 public class PacBeanHistoriaC implements Serializable {
-	private static final long serialVersionUID=1L;
+	private static final long serialVersionUID = 1L;
 	private PacPaciente paciente;
 	private int idCabecera;
 	private String fechaNacimiento;
@@ -46,36 +51,37 @@ public class PacBeanHistoriaC implements Serializable {
 	private String tratamiento;
 	private String estudios;
 	private String cuidados;
-	
+
 	@EJB
 	private PacManagerPaciente pacManagerPaciente;
+	@EJB
+	private MedManagerMedico medManagerMedico;
 	
-	
-	
+	@Inject
+	private ReserBeanCita reserBeanCita;
+
 	@PostConstruct
 	public void init() {
 		System.out.println("Init() HC");
 		System.out.println(paciente);
-		if(paciente!=null) {
+		if (paciente != null) {
 			getCabeceraHc();
 		}
 	}
-	
-	
-	
+
 	public String actionHistoriaClinica(PacPaciente pacienteC) {
-		paciente=pacienteC;
-	//	setPaciente(paciente);
-		System.out.println("Id paciente sele: "+paciente.getIdPaciente());
-		if(paciente!=null) {
+		paciente = pacienteC;
+		// setPaciente(paciente);
+		System.out.println("Id paciente sele: " + paciente.getIdPaciente());
+		if (paciente != null) {
 			getCabeceraHc();
 		}
 		return "historiaClinica?faces-redirect=true";
 	}
-	
+
 	public String actionGuardarCabeceraHc() {
-		//PacPaciente paciente=pacManagerPaciente.findPacienteById(17);
-		//System.out.println(beanCita.getIdPaciente());
+		// PacPaciente paciente=pacManagerPaciente.findPacienteById(17);
+		// System.out.println(beanCita.getIdPaciente());
 		System.out.println(fechaNacimiento);
 		System.err.println(peso);
 		System.err.println(altura);
@@ -92,26 +98,26 @@ public class PacBeanHistoriaC implements Serializable {
 		getCabeceraHc();
 		return "";
 	}
-	
+
 	public void getCabeceraHc() {
-		pacCabeceraHc=pacManagerPaciente.getPacienteCabeceraHc(paciente.getIdPaciente());
+		pacCabeceraHc = pacManagerPaciente.getPacienteCabeceraHc(paciente.getIdPaciente());
 		System.out.println(pacCabeceraHc);
-		if(pacCabeceraHc!=null) {
-			fechaNacimiento=pacCabeceraHc.getFechaNacimiento();
-			peso=Double.parseDouble(pacCabeceraHc.getPeso().toString());
-			altura=Double.parseDouble(pacCabeceraHc.getAltura().toString());
-			genero=pacCabeceraHc.getGenero();
-			ocupacion=pacCabeceraHc.getOcupacion();
-			idCabecera=pacCabeceraHc.getIdCabecera();
-			existeCab=true;
-		}else {
-			existeCab=false;
+		if (pacCabeceraHc != null) {
+			fechaNacimiento = pacCabeceraHc.getFechaNacimiento();
+			peso = Double.parseDouble(pacCabeceraHc.getPeso().toString());
+			altura = Double.parseDouble(pacCabeceraHc.getAltura().toString());
+			genero = pacCabeceraHc.getGenero();
+			ocupacion = pacCabeceraHc.getOcupacion();
+			idCabecera = pacCabeceraHc.getIdCabecera();
+			existeCab = true;
+		} else {
+			existeCab = false;
 		}
-		
+
 	}
-	
+
 	public String actionUpdateCabeceraHc() {
-		PacCabeceraHc cabeceraCargar=new PacCabeceraHc();
+		PacCabeceraHc cabeceraCargar = new PacCabeceraHc();
 		cabeceraCargar.setIdCabecera(idCabecera);
 		cabeceraCargar.setPacPaciente(paciente);
 		cabeceraCargar.setFechaNacimiento(fechaNacimiento);
@@ -125,17 +131,42 @@ public class PacBeanHistoriaC implements Serializable {
 		} catch (Exception e) {
 			JSFUtil.crearMensajeError("Error al actualizar");
 		}
-		
+
 		return "";
 	}
-	
+
 	public void actionNuevaHistoriaC() {
-		System.out.println("Ac nueva HC: "+newHistoriaC);
-		if(newHistoriaC) {
-			newHistoriaC=false;
-		}else {
-			newHistoriaC=true;
+		System.out.println("Ac nueva HC: " + newHistoriaC);
+		if (newHistoriaC) {
+			newHistoriaC = false;
+		} else {
+			newHistoriaC = true;
 		}
+	}
+
+	public String actionGuardarHistoriC() {
+		System.out.println("actionGuardarHistoriaC()");
+		fechaAtencion = new Date();
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(fechaAtencion);
+		@SuppressWarnings("deprecation")
+		Time ti = new Time(fechaAtencion.getTime());
+		horaAtencion = ti;
+		try {
+			PacCabeceraHc cabecera;
+			cabecera = pacManagerPaciente.findCabeceraHcById(idCabecera);
+			idMedico=reserBeanCita.getIdMedico();
+			System.out.println("Id médico: "+idMedico);
+			MedMedico medico;
+			medico = medManagerMedico.findMedicoById(idMedico);
+			pacManagerPaciente.ingresarPacHistoriaC(cabecera, fechaAtencion, horaAtencion, medico, cama, motivoConsulta,
+					enfermedadActual, diagnostico, evolucionMedica, plan, tratamiento, estudios, cuidados);
+			JSFUtil.crearMensajeInfo("Se creo historia clínica");
+		} catch (Exception e) {
+			JSFUtil.crearMensajeError(e.getMessage());
+		}
+
+		return "";
 	}
 
 	public PacPaciente getPaciente() {
@@ -186,211 +217,140 @@ public class PacBeanHistoriaC implements Serializable {
 		this.ocupacion = ocupacion;
 	}
 
-
-
 	public PacCabeceraHc getPacCabeceraHc() {
 		return pacCabeceraHc;
 	}
-
-
 
 	public void setPacCabeceraHc(PacCabeceraHc pacCabeceraHc) {
 		this.pacCabeceraHc = pacCabeceraHc;
 	}
 
-
-
 	public boolean isExisteCab() {
 		return existeCab;
 	}
-
-
 
 	public void setExisteCab(boolean existeCab) {
 		this.existeCab = existeCab;
 	}
 
-
-
 	public boolean isNewHistoriaC() {
 		return newHistoriaC;
 	}
-
-
 
 	public void setNewHistoriaC(boolean newHistoriaC) {
 		this.newHistoriaC = newHistoriaC;
 	}
 
-
-
 	public int getIdCabecera() {
 		return idCabecera;
 	}
-
-
 
 	public void setIdCabecera(int idCabecera) {
 		this.idCabecera = idCabecera;
 	}
 
-
-
 	public Date getFechaAtencion() {
 		return fechaAtencion;
 	}
-
-
 
 	public void setFechaAtencion(Date fechaAtencion) {
 		this.fechaAtencion = fechaAtencion;
 	}
 
-
-
 	public Time getHoraAtencion() {
 		return horaAtencion;
 	}
-
-
 
 	public void setHoraAtencion(Time horaAtencion) {
 		this.horaAtencion = horaAtencion;
 	}
 
-
-
 	public MedMedico getMedico() {
 		return medico;
 	}
-
-
 
 	public void setMedico(MedMedico medico) {
 		this.medico = medico;
 	}
 
-
-
 	public int getIdMedico() {
 		return idMedico;
 	}
-
-
 
 	public void setIdMedico(int idMedico) {
 		this.idMedico = idMedico;
 	}
 
-
-
 	public String getCama() {
 		return cama;
 	}
-
-
 
 	public void setCama(String cama) {
 		this.cama = cama;
 	}
 
-
-
 	public String getMotivoConsulta() {
 		return motivoConsulta;
 	}
-
-
 
 	public void setMotivoConsulta(String motivoConsulta) {
 		this.motivoConsulta = motivoConsulta;
 	}
 
-
-
 	public String getEnfermedadActual() {
 		return enfermedadActual;
 	}
-
-
 
 	public void setEnfermedadActual(String enfermedadActual) {
 		this.enfermedadActual = enfermedadActual;
 	}
 
-
-
 	public String getDiagnostico() {
 		return diagnostico;
 	}
-
-
 
 	public void setDiagnostico(String diagnostico) {
 		this.diagnostico = diagnostico;
 	}
 
-
-
 	public String getEvolucionMedica() {
 		return evolucionMedica;
 	}
-
-
 
 	public void setEvolucionMedica(String evolucionMedica) {
 		this.evolucionMedica = evolucionMedica;
 	}
 
-
-
 	public String getPlan() {
 		return plan;
 	}
-
-
 
 	public void setPlan(String plan) {
 		this.plan = plan;
 	}
 
-
-
 	public String getTratamiento() {
 		return tratamiento;
 	}
-
-
 
 	public void setTratamiento(String tratamiento) {
 		this.tratamiento = tratamiento;
 	}
 
-
-
 	public String getEstudios() {
 		return estudios;
 	}
-
-
 
 	public void setEstudios(String estudios) {
 		this.estudios = estudios;
 	}
 
-
-
 	public String getCuidados() {
 		return cuidados;
 	}
 
-
-
 	public void setCuidados(String cuidados) {
 		this.cuidados = cuidados;
 	}
-	
-	
-	
 
 }
